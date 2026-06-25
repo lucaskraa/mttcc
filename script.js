c024be6bad5bd1359a81190b45fb617aec432b99
lucaskraa <lucas.skraba@escola.pr.gov.br>
2026-06-25 15:32:29 -0300

    Update API_BASE_URL to include '/api' suffix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CONFIG = {
-  API_BASE_URL: "https://mttcc.onrender.com",
+  API_BASE_URL: "https://mttcc.onrender.com/api"
   TOKEN_KEY: "bookshare_token",
   REQUEST_TIMEOUT: 25000,
   SEARCH_DELAY: 260
